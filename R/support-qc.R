8b50d2ee4147f08e21677e70620f9fe89019475e
giannimonaco <mongianni1@gmail.com>
2022-01-31 20:34:07 +0100

    fixed addQC function

--- R/support-qc.R
+++ R/support-qc.R
@@ -46,7 +46,8 @@ addQC <- function(QCvector, remove_from, sub_exprs, params, keyval){
     sub_exprs <- cbind(sub_exprs, QCvector)
     attr(sub_exprs, "ranges") <- rs
     NN <- as.numeric(keyval["$PAR"]) + 1
-    names(dimnames(sub_exprs)[[2]])[dim(sub_exprs)[2]] <- paste0("$P",NN, "N")
+    # names(dimnames(sub_exprs)[[2]]) <- sprintf("$P%sN", 1:NN)
+    names(dimnames(sub_exprs)[[2]])[dim(sub_exprs)[2]] <- paste0("$P",NN, "N")   
     pnr <- paste0("$P", NN, "R")
     pnb <- paste0("$P", NN, "B")
     pne <- paste0("$P", NN, "E")
@@ -59,7 +60,7 @@ addQC <- function(QCvector, remove_from, sub_exprs, params, keyval){
     rownames(o)[length(o[,1])] <- paste("$P", NN, sep = "")
     
     outFCS <- new("flowFrame", exprs=sub_exprs, parameters=new("AnnotatedDataFrame",o), description=keyval)
-    keyword(outFCS)[pnr] <- "20000"
+    keyword(outFCS)[pnr] <- "20000"   # max(20000, keyword(outFCS)$`$P1R`)
     keyword(outFCS)[pnb] <- "32"   # keyword(outFCS)$`$P1B`
     keyword(outFCS)[pne] <- "0,0"
     keyword(outFCS)[pnn] <- paste0("remove_from_", remove_from)
